1ae9333692aed45d16fcb5887ca715cdd2609edd
MyName <kubelet@example.com>
2021-03-25 18:13:40 +0000

    Registry DNS

--- api/integration/api-spec.js
+++ api/integration/api-spec.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var base_url = "http://api-ingress:8080/";    
 
+
 describe("When testing 'api/products/1'", function(){
     it("should respond with entry for 'resistor'", function(done) {
         request(base_url + 'api/products/1', function(error, response, body){
